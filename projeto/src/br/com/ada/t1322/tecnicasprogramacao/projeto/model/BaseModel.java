fced13b75e9406e98c9c23cb54fc2ade19233ef4
Marcel Tanuri <marcel.tanuri@gmail.com>
2025-03-12 20:22:18 -0300

    add packages

---
+++ projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/model/BaseModel.java
@@ -0,0 +1,4 @@
+package br.com.ada.t1322.tecnicasprogramacao.projeto.model;
+
+public interface BaseModel {
+}
---
+++ projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/view/BaseView.java
@@ -0,0 +1,4 @@
+package br.com.ada.t1322.tecnicasprogramacao.projeto.view;
+
+public interface BaseView {
+}
