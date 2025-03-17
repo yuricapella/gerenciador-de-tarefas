45cee4634e306431b917bf7c239aed27714f5945
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-17 20:22:11 -0300

    feat: adiciona TaskReposityImpl

---
+++ projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/repository/TaskRepositoryImpl.java
@@ -0,0 +1,49 @@
+package br.com.ada.t1322.tecnicasprogramacao.projeto.repository;
+
+import br.com.ada.t1322.tecnicasprogramacao.projeto.model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class TaskRepositoryImpl implements TaskRepository {
+    List<Task> tasks = new ArrayList<>();
+    private static long count = 1;
+
+    public Task save(Task task){
+        task.setId(count++);
+        tasks.add(task);
+        return task;
+    }
+
+    public List<Task> findAll(){
+        return tasks;
+    }
+
+    public List<Task> findByStatus(String status){
+        return tasks.stream().filter(task -> task.getStatus().getDescricao().equals(status)).toList();
+    }
+
+    @Override
+    public List<Task> findByStatus(Task.Status status) {
+        return tasks.stream().filter(task -> task.getStatus().equals(status)).toList();
+    }
+
+    @Override
+    public List<Task> findBy(Predicate<Task> predicate) {
+        return tasks.stream().filter(predicate).toList();
+    }
+
+    public Optional<Task> findById(Long id){
+        return tasks.stream().filter(task->task.getId().equals(id)).findFirst();
+    }
+
+    public boolean deleteById(Long id){
+        return tasks.stream().anyMatch(task->task.getId().equals(id));
+    }
+
+    public void deleteAll(){
+        this.tasks.clear();
+    }
+}
