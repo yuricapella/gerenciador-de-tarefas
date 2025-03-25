5761ba1aedc08f7ece7977bc751beebe50030270
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-24 21:07:51 -0300

    corrigido arquivos, agora somente com os arquivos do professor

--- projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/service/notification/Notifier.java
+++
@@ -1,6 +0,0 @@
-package br.com.ada.t1322.tecnicasprogramacao.projeto.service.notification;
-
-public interface Notifier {
-    void start();
-    void stop();
-}
--- projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/service/validation/TaskValidation.java
+++
@@ -1,7 +0,0 @@
-package br.com.ada.t1322.tecnicasprogramacao.projeto.service.validation;
-
-import br.com.ada.t1322.tecnicasprogramacao.projeto.model.Task;
-
-public interface TaskValidation {
-    void validate(Task task);
-}
