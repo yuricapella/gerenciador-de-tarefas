a603da206da415d07833b11b70276c956ba35263
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-28 14:20:44 -0300

    adiciona nova tarefa com o status bloqueado desde o inicio do programa

--- projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/util/SampleDataInitializer.java
+++ projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/util/SampleDataInitializer.java
@@ -11,11 +11,13 @@ public class SampleDataInitializer {
     public static void initializeSampleTasks(TaskController controller) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        controller.createTask("Reunião de equipe", "Discutir metas da semana", LocalDate.now().format(formatter), Task.Status.PENDENTE);
-
         Task taskRelatorio = controller.createTask("Entrega do relatório", "Relatório financeiro mensal", LocalDate.now().plusDays(1).format(formatter), Task.Status.EM_ANDAMENTO);
         controller.updateTaskStatus(taskRelatorio.getId(), Task.Status.CONCLUIDO);
 
+        Task taskBloqueada = controller.createTask("Tarefa Bloqueada", "Tarefa bloqueada por alguma razão", LocalDate.now().format(formatter), Task.Status.PENDENTE);
+        controller.updateTaskStatus(taskBloqueada.getId(), Task.Status.BLOQUEADO);
+
+        controller.createTask("Reunião de equipe", "Discutir metas da semana", LocalDate.now().format(formatter), Task.Status.PENDENTE);
         controller.createTask("Atualizar backlog", "Revisar e priorizar tarefas", LocalDate.now().plusDays(2).format(formatter), Task.Status.PENDENTE);
         controller.createTask("Call com cliente", "Apresentação de progresso do projeto", LocalDate.now().plusDays(3).format(formatter), Task.Status.EM_ANDAMENTO);
         controller.createTask("Planejamento sprint", "Definição das atividades para a próxima sprint", LocalDate.now().plusDays(4).format(formatter), Task.Status.PENDENTE);
